d96fbb8457b92147180c2fedd93bf587be9c1104
soone chen <fengyue15@163.com>
2010-06-22 22:11:44 +0800

    测试临时文件

---
+++ Cache/tpl_c/4ece063deca149f947054b03149b7dfc12ef4b5d.file.footer.tpl.php
@@ -0,0 +1,27 @@
+<?php /* Smarty version Smarty3-b8, created on 2010-06-22 22:11:25
+         compiled from "/media/work_study/work/soone/xgm/./Www/template/default/footer.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:2299818364c20c48dc53fb6-21921806%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '4ece063deca149f947054b03149b7dfc12ef4b5d' => 
+    array (
+      0 => '/media/work_study/work/soone/xgm/./Www/template/default/footer.tpl',
+      1 => 1276703141,
+    ),
+  ),
+  'nocache_hash' => '2299818364c20c48dc53fb6-21921806',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+)); /*/%%SmartyHeaderCode%%*/?>
+	</div>
+</div>
+<div class="container">
+    <div id="cr">
+    copyright&nbsp;&nbsp;by&nbsp;&nbsp;西瓜猫&nbsp;&nbsp;&nbsp;&nbsp;designed&nbsp;&nbsp;by&nbsp;&nbsp;soone&nbsp;&nbsp;email:fengyue15#163.com
+    </div>
+</div>
+</body>
+</html>
