16c22093e1a3cd94f64b37c72180aca625f54479
soone chen <fengyue15@163.com>
2010-09-11 14:48:59 +0800

    折腾死了

--- Cache/tpl_c/%%32^320^32007359%%User_suplist.tpl.php
+++
@@ -1,2 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2010-09-11 14:33:37
-         compiled from User_suplist.tpl */ ?>
--- Cache/tpl_c/%%88^886^886075F1%%User_index.tpl.php
+++
@@ -1,5 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2010-09-11 14:21:31
-         compiled from User_index.tpl */ ?>
-<?php echo $this->_tpl_vars['a']; ?>
-
-xxxxxxxxxxxxxxxxxxxxx
